c06efba0a10f31179efa24100b57ade6dd54cf36
cabiste <ahmedjj354@gmail.com>
2023-06-24 21:12:51 +0100

    minor changes

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,11 +12,15 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    // serves the swagger UI at the app's root (url.com/index.html)
+    options.RoutePrefix = string.Empty;
+    //todo: why is it not workingg :(
+    options.InjectStylesheet("/Assets/Swagger/custom.css");
+});
 
 app.UseHttpsRedirection();
 
